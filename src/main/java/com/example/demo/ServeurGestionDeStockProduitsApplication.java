df463839ca94a141446a5030b99ba05412df8ad5
abderrazakiazzi <abdou.iazzi@gmail.com>
2018-11-23 18:25:24 +0000

    first commit

---
+++ src/main/java/com/example/demo/ServeurGestionDeStockProduitsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionDeStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionDeStockProduitsApplication.class, args);
+	}
+}
